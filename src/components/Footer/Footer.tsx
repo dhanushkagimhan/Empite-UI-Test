24cc404859de665c54c714483fae1be6bd36e011
Dhanushka Gimhan <dhanushkagimhan@gmail.com>
2024-03-16 20:25:53 +0530

    created footer section

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer>
       <div className="bg-black">
         <div className="container mx-auto pt-[109px] pb-[56px] px-2 text-white">
-          <div className="grid grid-cols-4 gap-4">
+          <div className="grid 2xl:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-10">
             <div>
               <a href="#">
                 <img src={LogoWhite} alt="Empite Logo" />
@@ -43,11 +43,11 @@ export default function Footer() {
             <div>
               <div>
                 <div className="text-[22px] text-title-grey">Stay in touch</div>
-                <div className="text-[22px] mt-10 flex justify-between items-center pt-[21px] pb-[25px] px-[28px] bg-box-grey rounded-[5px]">
+                <div className="text-[22px] mt-10 flex justify-between max-[360px]:flex-col min-[360px]:items-center gap-2 p-6 bg-box-grey rounded-[5px]">
                   <div>
                     <input
                       type="text"
-                      className="max-w-[200px] bg-box-grey p-2 outline-none"
+                      className="max-w-[200px] bg-box-grey py-2 outline-none text-[20px]"
                       placeholder="Your email address"
                     />
                   </div>
@@ -59,7 +59,9 @@ export default function Footer() {
             </div>
           </div>
           <div className="w-full mt-[75px] bg-box-grey h-[0.1px]"></div>
-          <div className="mt-[50px]">s</div>
+          <div className="mt-[50px] text-[22px] font-extralight">
+            © 2020, Empite Pty Ltd
+          </div>
         </div>
       </div>
     </footer>
